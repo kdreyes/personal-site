3e747b0b09b0fd7fce89e03b9df868e5c6e633e8
kdreyes <katrina.mdelosreyes@gmail.com>
2021-01-07 11:10:27 -0500

    added blog post

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,6 +1,23 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'What\'s with the ampersand?',
+    subtitle: '',
+    link: 'https://medium.com/andtech/whats-with-the-ampersand-869328a4ec',
+    image: '/images/blogbackground.png',
+    date: '2021-01-07',
+    desc: 'With less than 8 months left to my degree in CS and math, '
+      + 'I panicked thinking, “This is really it for tech, huh? Just '
+      + 'coding away in Java or C++ to make this button do that or to '
+      + 'maybe make this thing faster using a ‘cool algorithm’?” As I '
+      + 'sat in the lounge of my off-campus apartment (shoutout to The '
+      + 'Varsity) procrastinating on some CMSC451 problem most likely, '
+      + 'I knew — I just knew — that this couldn’t possibly be it. And '
+      + 'that there was a way to make a bigger impact and to make tech '
+      + 'fun and sustainable and something I could see myself doing for '
+      + 'a long, long time. I just couldn’t put my finger on it.',
+  },
+  {
     title: 'Year One: Listen to Your Gut',
     subtitle: '',
     link: 'https://medium.com/@katrina.mdelosreyes/year-one-listen-to-your-gut-384aa41c7ea5',
